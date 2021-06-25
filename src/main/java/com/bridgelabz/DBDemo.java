4e49eb0e8c78d992651c9751ddf3e253793bf486
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-06-25 16:03:38 +0530

    ADDED:Java program connect to database

--- src/main/java/com/bridgelabz/DBDemo.java
+++ src/main/java/com/bridgelabz/DBDemo.java
@@ -1,8 +1,41 @@
 package com.bridgelabz;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
 public class DBDemo {
     public static void main(String args[])
     {
-        System.out.println("Welcome to Employee Payroll Service Program");
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String userName = "root";
+        String password = "Priyanka@1208";
+        Connection connection;
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver loaded");
+        }catch (ClassNotFoundException e) {
+            throw new IllegalStateException("Cannot find driver", e);
+        }
+        listDrivers();
+        try {
+            System.out.println("connecting to database" + jdbcURL);
+            connection = DriverManager.getConnection(jdbcURL, userName, password);
+            System.out.println("Connection is successful" + connection);
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private static void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements())
+        {
+            Driver driverClass = (Driver) driverList.nextElement();
+            System.out.println(" " + driverClass.getClass().getName());
+        }
     }
 }
